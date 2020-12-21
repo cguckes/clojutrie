e81becfc59ad940174bf2193f664b00ce5523814
Christopher Thonfeld-Guckes <christopher.guckes@torq-dev.de>
2020-12-21 17:28:46 +0100

    Add deploy repository and group name

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject clojutrie "0.0.1"
+(defproject ctg/clojutrie "0.0.1"
   :description "A simple search trie build on native clojure"
   :url "https://github.com/cguckes/clojutrie"
   :min-lein-version "2.9.0"
   :license {:name "The MIT License"
             :url  "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :aot [clojutrie.core]
+  :resource-paths ["resources"]
+  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]]
   :repl-options {:init-ns clojutrie.core})
